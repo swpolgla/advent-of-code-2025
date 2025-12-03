0a500d9731f3bf15bffd0bdb6ac466d15f63d212
Steven Polglase <swpolgla@pm.me>
2025-12-02 21:40:32 -0500

    Day 2 Optimization

--- Sources/advent-of-code-2025/day_2.swift
+++ Sources/advent-of-code-2025/day_2.swift
@@ -4,25 +4,39 @@ func solve_day_2(input: String) async -> (Int, Int) {
     var invalid_ids: Int = 0
     var complex_invalid_ids: Int = 0
 
+    var chunks: Set<Int> = []
+
     let ranges: [String.SubSequence] = input.split(separator: ",")
     for range: String.SubSequence in ranges {
         let range_split: [Substring.SubSequence] = range.split(separator: "-")
         for i: Int in (Int(range_split[0]) ?? -1)...(Int(range_split[1]) ?? -1) {
-            let num: String = String(i)
+            var digits: [Int] = []
+            var num: Int = i
+            while num > 0 {
+                digits.append(num % 10)
+                num /= 10
+            }
+            var p2_done: Bool = false
             for j: Int in 1...5 {
-                if num.count % j == 0 && num.count != j {
-                    var chunks: Set<String.SubSequence> = []
-                    for k: Int in 0..<(num.count / j) {
-                        let start_index: String.Index = num.index(num.startIndex, offsetBy: k * j)
-                        let end_index: String.Index = num.index(num.startIndex, offsetBy: k * j + j - 1)
-                        chunks.insert(num[start_index...end_index])
+                if digits.count % j == 0 && digits.count != j {
+                    chunks.removeAll()
+                    for k: Int in 0..<(digits.count / j) {
+                        var reconstructed_chunk: Int = 0
+                        for idx: Int in (k * j)...(k * j + j - 1) {
+                            reconstructed_chunk += digits[idx]
+                            reconstructed_chunk *= 10
+                        }
+                        reconstructed_chunk /= 10
+                        chunks.insert(reconstructed_chunk)
                     }
                     if chunks.count == 1 {
-                        if j * 2 == num.count {
+                        if j * 2 == digits.count {
                             invalid_ids += i
                         }
-                        complex_invalid_ids += i
-                        break
+                        if !p2_done {
+                            complex_invalid_ids += i
+                        }
+                        p2_done = true
                     }
                 }
             }

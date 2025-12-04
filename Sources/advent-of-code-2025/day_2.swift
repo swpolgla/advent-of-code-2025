01b298842ad6de26cf6d4457bfb73d7bb32da1d8
Steven Polglase <swpolgla@pm.me>
2025-12-04 02:15:46 -0500

    Further Day 2 Optimization

--- Sources/advent-of-code-2025/day_2.swift
+++ Sources/advent-of-code-2025/day_2.swift
@@ -10,27 +10,28 @@ func solve_day_2(input: String) async -> (Int, Int) {
     for range: String.SubSequence in ranges {
         let range_split: [Substring.SubSequence] = range.split(separator: "-")
         for i: Int in (Int(range_split[0]) ?? -1)...(Int(range_split[1]) ?? -1) {
-            var digits: [Int] = []
+            var digits: [16 of Int] = [16 of Int].init(repeating: 0)
             var num: Int = i
+            var count: Int = 0
             while num > 0 {
-                digits.append(num % 10)
+                digits[count] = num % 10
                 num /= 10
+                count += 1
             }
             var p2_done: Bool = false
             for j: Int in 1...5 {
-                if digits.count % j == 0 && digits.count != j {
+                if count % j == 0 && count != j {
                     chunks.removeAll()
-                    for k: Int in 0..<(digits.count / j) {
+                    for k: Int in 0..<(count / j) {
                         var reconstructed_chunk: Int = 0
                         for idx: Int in (k * j)...(k * j + j - 1) {
-                            reconstructed_chunk += digits[idx]
                             reconstructed_chunk *= 10
+                            reconstructed_chunk += digits[idx]
                         }
-                        reconstructed_chunk /= 10
                         chunks.insert(reconstructed_chunk)
                     }
                     if chunks.count == 1 {
-                        if j * 2 == digits.count {
+                        if j * 2 == count {
                             invalid_ids += i
                         }
                         if !p2_done {
